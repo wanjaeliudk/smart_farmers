cef2c451718005b882a5a0f776fb03fa74057dac
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-05-20 09:24:04 +0300

    add branch changes to test

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,6 +15,30 @@ document.querySelector('#login-btn').onclick = () =>{
     navbar.classList.remove('active');
 }
 
+
+let attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if ( username === "Formget@gmail.com" && password === "formget#123"){
+        alert ("Login successfully");
+        window.location = "../../events.html"; // Redirecting to other page.
+        return false;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left "+attempt+" attempt;");
+// Disabling fields after 3 attempts.
+        if( attempt === 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
+
 let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
